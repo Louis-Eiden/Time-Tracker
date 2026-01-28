f6353f3a4bb109df3004fe2b276ed8fa92f4dddf
Louis-Eiden <eidenlouis.mt@gmail.com>
2026-01-28 11:14:31 +0100

    resized icons

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ import type { ExpoConfig } from "expo/config";
 const config: ExpoConfig = {
   name: "Time-Tracker",
   slug: "Time-Tracker",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
